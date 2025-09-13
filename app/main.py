b159d1fe6ab4c82b61fd568be4e23fba462651ff
Soyam paul <soyampaul34@gmail.com>
2025-09-14 01:36:10 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes import router
-
-app = FastAPI(title="YOLOv8 Garbage Detection API 🚀")
-app.include_router(router)
-
-@app.get("/")
-def root():
-    return {"status": "API running"}
